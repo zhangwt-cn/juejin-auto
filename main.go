94f17838f415522fa6fee8f8036a4f278badc2a7
zhangwt <im.zhangwt@gmail.com>
2022-10-11 10:35:01 +0800

    fix: 修改掘金 cookie 获取方式

--- main.go
+++ main.go
@@ -3,26 +3,25 @@ package main
 import (
 	"juejin-auto/model"
 	"juejin-auto/service"
-	"log"
 	"os"
-
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
 	//定义一个结构体变量
-	var config model.Config 
-	log.Println(os.Getenv("JUEJIN_COOKIE")) 
+	// var config model.Config 
+	// log.Println(os.Getenv("JUEJIN_COOKIE")) 
 
-	// 读取yaml文件
-	byteArr, err := os.ReadFile("config.yaml")
-	if err != nil {
-		log.Print(err)
-	}
-	// yaml文件内容映射到结构体中
-	err1:= yaml.Unmarshal(byteArr, &config)
-	if err1!=nil{
-		log.Println("error")
-	}
+	// // 读取yaml文件
+	// byteArr, err := os.ReadFile("config.yaml")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// // yaml文件内容映射到结构体中
+	// err1:= yaml.Unmarshal(byteArr, &config)
+	// if err1!=nil{
+	// 	log.Println("error")
+	// }
+	
+	config := model.Config{os.Getenv("JUEJIN_COOKIE")}
 	service.CheckIn(config)
 }
